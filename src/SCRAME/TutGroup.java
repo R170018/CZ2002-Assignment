97cafe9c2310518ac971d0308edc55717f25dd26
Jiang Nan <C170187@e.ntu.edu.sg>
2018-10-26 11:38:32 +0800

    JN

--- src/SCRAME/TutGroup.java
+++ src/SCRAME/TutGroup.java
@@ -1,4 +1,30 @@
 package SCRAME;
 
 public class TutGroup {
+	private String groupName;
+	private int vacancy;
+	private Student[] student=new Student[15];
+	int studentIndex=0;
+	
+	public TutGroup(String groupName) {
+		this.groupName=groupName;
+		vacancy=10;
+	}
+	
+	public String getTutGroupName() {
+		return groupName;
+	}
+	
+	public void registerForTutGroup(Student student) {
+		if (vacancy>0) {
+			this.student[studentIndex]=student;
+		}
+		vacancy--;
+		
+	}
+	public void getVacancy() {
+		System.out.println("Vacancy for utorial class "+groupName+": "+vacancy+"/10");
+	}
+	
+
 }
