3630fd512ec1536669cc98d4109f60d814fbbf83
Xiaochen Jia <cjasonjia@gmail.com>
2018-10-26 11:54:46 +0800

    Update Assessment

--- src/SCRAME/Assessment.java
+++ src/SCRAME/Assessment.java
@@ -1,31 +1,44 @@
 package SCRAME;
+
 public class Assessment{
+	public static final int MAX_SUBASSESSMENT = 4;
 	private string name;
 	private float percentage;
-	private Assessment[4] subAssessment;
+	private Assessment[MAX_SUBASSESSMENT] subAssessment;
 	private int numOfSubAssessment = 0;
+	private int maxMark;
 
 	public setName(string name){
 		this.name = name;
 	}
 
-	public setPercentage(float percentage){
+	public int setPercentage(float percentage){
 		if(percentage > 0 && percentage <=1){
 			this.percentage = percentage;
+			return 1;
 		}
 		else{
 			System.out.println("Percentage needs to be a number between 0 and 1");
+			return -1;
 		}
 	}
 
-	public setNumOfSubAssessment(int number){
-		numOfSubAssessment = number;
+	public int setNumOfSubAssessment(int number){
+		if(number <= MAX_SUBASSESSMENT){
+			numOfSubAssessment = number;
+			return 1;
+		}
+		else{
+			System.out.println("Number of sub-assessment cannot exceed " + MAX_SUBASSESSMENT);
+			return -1;
+		}
 	}
 
-	public setSubAssessments(int index, string name, float percentage){
+	public setSubAssessments(int index, string name, float percentage, int maxMark){
 		if(index < numOfSubAssessment){
 			subAssessment[index].setName(name);
 			subAssessment[index].setPercentage(percentage);
+			subAssessment[index].setMaxMark(maxMark);
 		}
 		else{
 			System.out.println("Index go beyond allowed number of sub-assessment.");
