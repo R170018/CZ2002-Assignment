ca0751cfc96e6619afa7eaf4361e092ebc0f8c69
Jiang Nan <C170187@e.ntu.edu.sg>
2018-10-26 11:22:39 +0800

    JN

--- src/SCRAME/Course.java
+++ src/SCRAME/Course.java
@@ -1,4 +1,11 @@
 package SCRAME;
 
 public class Course {
+    private Assessment assessment;
+	private int mark;
+	private AssessmentGrade[] subAssessmentGrade = new AssessmentGrade[4] ;
+	private int numOfSubAssessment;
+	
+	
+
 }
